c3ab3feab0cc2685455fd9f7e9fdb16647ba3ffb
Adam Roderick <adam@datateer.com>
2023-08-14 17:04:51 -0600

    initial commit

---
+++ tap_freshservice/tap.py
@@ -0,0 +1,58 @@
+"""Freshservice tap class."""
+
+from __future__ import annotations
+
+from singer_sdk import Tap
+from singer_sdk import typing as th  # JSON schema typing helpers
+
+# TODO: Import your custom stream types here:
+from tap_freshservice import streams
+
+
+class TapFreshservice(Tap):
+    """Freshservice tap class."""
+
+    name = "tap-freshservice"
+
+    # TODO: Update this section with the actual config values you expect:
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "auth_token",
+            th.StringType,
+            required=True,
+            secret=True,  # Flag config as protected.
+            description="The token to authenticate against the API service",
+        ),
+        th.Property(
+            "project_ids",
+            th.ArrayType(th.StringType),
+            required=True,
+            description="Project IDs to replicate",
+        ),
+        th.Property(
+            "start_date",
+            th.DateTimeType,
+            description="The earliest record date to sync",
+        ),
+        th.Property(
+            "api_url",
+            th.StringType,
+            default="https://api.mysample.com",
+            description="The url for the API service",
+        ),
+    ).to_dict()
+
+    def discover_streams(self) -> list[streams.FreshserviceStream]:
+        """Return a list of discovered streams.
+
+        Returns:
+            A list of discovered streams.
+        """
+        return [
+            streams.GroupsStream(self),
+            streams.UsersStream(self),
+        ]
+
+
+if __name__ == "__main__":
+    TapFreshservice.cli()
